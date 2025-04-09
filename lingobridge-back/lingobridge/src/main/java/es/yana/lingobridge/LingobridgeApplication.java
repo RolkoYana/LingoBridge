376ab8feca68676648db55361f409cef4436c4f2
Yana Rolko <rolkoyana@gmail.com>
2025-04-09 19:36:09 +0200

    crear backend para proyecto

---
+++ lingobridge-back/lingobridge/src/main/java/es/yana/lingobridge/LingobridgeApplication.java
@@ -0,0 +1,13 @@
+package es.yana.lingobridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingobridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingobridgeApplication.class, args);
+	}
+
+}
